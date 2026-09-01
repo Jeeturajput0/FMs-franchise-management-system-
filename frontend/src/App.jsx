0716cb7464b9eed16439cc0ad4abfc9e146048eb
Vishnu Kushwah <vishnukush10@gmail.com>
2026-09-01 15:20:53 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <div>
         <h1>the new page </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati accusantium assumenda voluptatem nulla adipisci! Fugit reiciendis mollitia nostrum officia odit expedita quis harum laudantium labore? Totam exercitationem itaque qui nihil?</p>
       </div>
     </>
   );
